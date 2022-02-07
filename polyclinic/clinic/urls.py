6c3e715ea505b8f60789dbfbdd455da5c6af1d59
ZioVio <anderkoval3@gmail.com>
2022-02-07 19:53:42 +0200

    BE handlers done

--- polyclinic/clinic/urls.py
+++ polyclinic/clinic/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('employment_department', views.employment_department, name='emp_dep'),
-    path('employment_department/<str:position_code>', views.employment_department, name='emp_dep'),
-    path('illnesses/', views.illnesses),
-    path('illnesses_by_drugs/', views.illnesses_by_drugs)
+    path('employment_department', views.employment_department_handler, name='emp_dep'),
+    path('employment_department/<str:position_code>', views.employment_department_handler, name='emp_dep'),
+    path('illnesses/', views.illnesses_handler),
+    path('illnesses_by_drugs/', views.illnesses_by_drugs_handler),
+    path('patients', views.patients_handler),
+    path('doctor_patients/<str:doctor_code>', views.doctor_patients_handler)
 ]
--- polyclinic/clinic/views.py
+++ polyclinic/clinic/views.py
@@ -1,7 +1,7 @@
 import json
 from django.core import serializers
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseNotFound
 
 # Create your views here.
 from clinic.models import Patient, Employee, Position, Illness, Drug
@@ -12,7 +12,7 @@ def index(request):
     return HttpResponse('Hello world')
 
 
-def employment_department(request, position_code=None):
+def employment_department_handler(request, position_code=None):
     print(position_code)
     positions = dict_from_queryset(
         Position.objects.all() if position_code is None else Position.objects.filter(code=position_code)
@@ -29,7 +29,7 @@ def employment_department(request, position_code=None):
     return HttpResponse(render(request, 'clinic/epm_dep.html', context))
 
 
-def illnesses(request):
+def illnesses_handler(request):
     ill = dict_from_queryset(Illness.objects.all())
     drugs = dict_from_queryset(Drug.objects.all())
     for drug in drugs:
@@ -41,7 +41,7 @@ def illnesses(request):
     return HttpResponse(ill)
 
 
-def illnesses_by_drugs(request):
+def illnesses_by_drugs_handler(request):
     ill = dict_from_queryset(Illness.objects.all())
     drugs = dict_from_queryset(Drug.objects.all())
     for drug in drugs:
@@ -54,3 +54,29 @@ def illnesses_by_drugs(request):
                     drug['cures'].append(il)
 
     return HttpResponse(drugs)
+
+
+def patients_handler(request):
+    """List of patients with illness and doctor with button do get all doctors patients"""
+    patients = dict_from_queryset(Patient.objects.all())
+    for p in patients:
+        p['doctor'] = dict_from_queryset(Employee.objects.filter(code=p['employee_code_id']))[0]
+        p['illness'] = dict_from_queryset(Illness.objects.filter(code=p['illness_code_id']))[0]
+
+    # todo replace this html with an actual one later
+    return HttpResponse(render(request, 'clinic/epm_dep.html', {'positions': patients}))
+
+
+def doctor_patients_handler(request, doctor_code=None):
+    """List of patients with illness and doctor"""
+    try:
+        doctor = Employee.objects.get(pk=doctor_code)
+    except Employee.DoesNotExist:
+        return HttpResponseNotFound(f'Cannot find doctor with code:{doctor_code}')
+
+    doctor_dict = doctor.__dict__
+    doctor_dict['patients'] = dict_from_queryset(
+        Patient.objects.filter(employee_code_id=doctor_dict['code'])
+    )
+    print(doctor_dict)
+    return HttpResponse(doctor_dict)
